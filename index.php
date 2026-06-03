c8af287ee001b942e78a6e8e2ed4a7642cb15cb8
Ruban Ivan <m2413041@edu.misis.ru>
2026-06-03 17:51:43 +0300

    First version. There are a lot of work to do.

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Стихотвория – Главная</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div class="page">
+            
+            <div class="headline">
+                <a href="index.php">Главная</a> <a href="authors.php">Авторы</a> <a href="poetry.php">Произведения</a> <a href="account.php">Аккаунт</a>
+            </div>
+
+            <div class = "center">
+                <h1>Стихотвория</h1>
+            </div>
+
+            <div class = "copyright">
+                Все права защищены © 2026
+            </div>
+            
+        </div>
+        
+        <script src="script.js"></script>
+    </body>
+</html>
